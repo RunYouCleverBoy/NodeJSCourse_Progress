d3169ec75f4bb0953356aa82696688f076e1e7b1
Shmuel Meymann <meymann@gmail.com>
2024-01-31 18:58:20 +0200

    Helmet

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const userRouter = require('./routes/userRouter');
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const app = express();
+
+// Set security HTTP headers
+app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// Limit request from same API
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
@@ -20,23 +25,30 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.static(`${__dirname}/public`));
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
 
+
+// Routes
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', userRouter);
 
-
+// Error handling
 app.all('*', (req, res, next) => {
     const err = new AppError(`Can't find ${req.originalUrl} on this server!`, 404);
     next(err);
 });
 
+// Global error handling middleware
 app.use(globalErrorHandler);
 
 module.exports = app;
