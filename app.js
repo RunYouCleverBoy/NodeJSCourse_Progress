5f396d47328e7c73a18e36cef306874675ea5468
Shmuel Meymann <meymann@gmail.com>
2024-01-31 18:25:33 +0200

    Added rate limit with the package express-rate-limit

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ const toursRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many request from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
 app.use(express.static(`${__dirname}/public`));
 app.use(express.json());
 
@@ -17,9 +31,6 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', userRouter);
 
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-}
 
 app.all('*', (req, res, next) => {
     const err = new AppError(`Can't find ${req.originalUrl} on this server!`, 404);
